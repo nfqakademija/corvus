1ca5e4338550b5d8df00fa3dd941a121a0c936fd
Vagrant User <vagrant@corvus.puphpet>
2015-11-12 17:15:12 +0000

    test Controller and form

--- src/Corvus/MainBundle/Controller/testController.php
+++ src/Corvus/MainBundle/Controller/testController.php
@@ -5,6 +5,7 @@ namespace Corvus\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class testController extends Controller
 {
@@ -12,10 +13,26 @@ class testController extends Controller
      * @Route("/Contacts")
      * @Template()
      */
-    public function ContactsAction()
-    {
+    public function ContactsAction(Request $request)
+    {	
+        $data = array(
+            'name' => ''
+            );
+	$form = $this->createFormBuilder($data)
+            ->add('name', 'text', array('label' => 'Įveskite adresą:'))
+            ->add('save', 'submit', array('label' => 'Rodyti temperatūrą'))
+            ->getForm();
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+            
+            $data = $form->getData();
+        }
+
         return array(
-                // ...
-            );    }
+            'form' => $form->createView(),
+	    'data' => $data['name']	
+        );
+	
+    }
 
 }
