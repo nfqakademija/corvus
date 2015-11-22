fc66167e8b7c11a2108fbc924692bbed020ddf6a
Andrius Vaskevicius <andrius@vaskevicius.lt>
2015-11-23 00:03:55 +0200

    Edit Dashboard Controller. Main information is sent and shown on dashboard

--- src/Corvus/DashboardBundle/Controller/DefaultController.php
+++ src/Corvus/DashboardBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Corvus\DashboardBundle\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -17,14 +18,38 @@ class DefaultController extends Controller
         $user = $this->container->get('security.context')->getToken()->getUser();
         $id = $user->getId();
 
-        $em = $this->getDoctrine()->getManager();
-
         $user = $this->getDoctrine()->getRepository('CorvusMainBundle:User')->find($id);
 
         $event_host_cnt = $user->getEventsHost()->count();
-        $even_cnt = $user->getEvents()->count();
+        $event_cnt = $user->getEvents()->count();
 
-        //return $this->render('CorvusMainBundle:welcome:dashboard.html.twig', array('user_id' => $id));
-        return array('name' => $id, 'count' => $event_host_cnt, 'ecount' => $even_cnt);
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT e
+            FROM EventBundle:Event e
+            JOIN e.users u
+            WHERE e.host = :uid OR u.id = :uid
+            ORDER BY e.endDateTime ASC
+            '
+        )->setParameter('uid' , $id);
+
+        $e = $query->getResult();
+
+        $c = array();
+        $o = array();
+        foreach ($e as $event)
+        {
+            $query = $em->createQuery(
+                    'SELECT COUNT (DISTINCT o.user)
+                    FROM EventBundle:Order o
+                    WHERE o.event = :event
+            '
+            )->setParameter('event', $event->getId());
+
+            $o[$event->getId()] = $query->getSingleScalarResult();
+        }
+
+        return array('name' => $id, 'hcount' => $event_host_cnt, 'ecount' => $event_cnt, 'events' => $e, 'ordered' => $o);
     }
 }
