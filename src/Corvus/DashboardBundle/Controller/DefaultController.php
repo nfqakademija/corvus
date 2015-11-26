753c312b8ce447584a8ef6577bc36b710e0fa68b
Andrius Vaskevicius <andrius@vaskevicius.lt>
2015-11-27 01:09:54 +0200

    Add buttons to dashboard. Routes missing

--- src/Corvus/DashboardBundle/Controller/DefaultController.php
+++ src/Corvus/DashboardBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/dashboard/")
+     * @Route("/dashboard/", name="dashboard")
      * @Template()
      */
     public function indexAction()
@@ -22,17 +22,6 @@ class DefaultController extends Controller
 
         $em = $this->getDoctrine()->getManager();
 
-       /* $query = $em->createQuery(
-            'SELECT e
-            FROM EventBundle:Event e
-            JOIN e.users u
-            WHERE e.host = :uid OR u.id = :uid
-            ORDER BY e.endDateTime ASC
-            '
-        )->setParameter('uid' , $id);
-
-        $e = $query->getResult();*/
-
         $query = $em->createQuery(
             'SELECT e
             FROM EventBundle:Event e
@@ -43,24 +32,37 @@ class DefaultController extends Controller
 
         $e = $query->getResult();
 
-        dump($e);
-        //exit;
-
-        /*$c = array();
-        $o = array();
-        foreach ($e as $event)
-        {
-            $query = $em->createQuery(
-                    'SELECT COUNT (DISTINCT o.user), o.event
-                    FROM EventBundle:Order o
-                    WHERE o.event = :event
-            '
-            )->setParameter('event', $event->getId());
-
-            $o[$event->getId()] = $query->getSingleScalarResult();
-            $o[] = ['event' => $event, 'ordered' => $query->getSingleScalarResult()];
-        }*/
-
+        //dump($e);
         return array('uid' => $id, 'events' => $e);
+    }
+
+    /**
+     * @Route("/suspend/{eventId}", name="suspend")
+     */
+    public function suspendAction($eventId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $event = $this->getDoctrine()->getRepository('EventBundle:Event')->find($eventId);
+        switch ($event->getStatus())
+        {
+            case 1:
+                $event->setStatus(2);
+                $em->flush();
+                return $this->redirectToRoute('dashboard');
+                break;
+            case 2:
+                $now = new \DateTime('now');
+                $endTime = $event->getEndDateTime();
+                if ($now->diff($endTime) > 0)
+                {
+                    $event->setStatus(1);
+                    $em->flush();
+                }
+                return $this->redirectToRoute('dashboard');
+                break;
+            default:
+                return $this->redirectToRoute('dashboard');
+
+        }
     }
 }
