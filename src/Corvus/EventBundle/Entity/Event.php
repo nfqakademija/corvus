b3af0247924180db279a34c93035a23a55e609fe
Andrius Vaskevicius <andrius@vaskevicius.lt>
2015-12-02 01:56:43 +0200

    Fix to Dashboard. Add authentication, and don't calculate removed food in order totals

--- src/Corvus/EventBundle/Entity/Event.php
+++ src/Corvus/EventBundle/Entity/Event.php
@@ -438,7 +438,7 @@ class Event
     {
         $eventSum = 0;
         foreach ($this->orders as $order) {
-            if ($order->getUser() != $this->host) {
+            if (($order->getUser() != $this->host) && (!$order->getIsRemoved())) {
                 $eventSum += $order->getPricePerUnit() * $order->getQuantity();
             }
         }
